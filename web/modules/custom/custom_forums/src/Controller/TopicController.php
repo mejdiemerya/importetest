4e819ce7d6718d56d0fedbada21687691a9dd5c4
ajedidi <asma.jedidi@emerya.fr>
2024-11-18 15:45:28 +0100

    Add topic forum

--- web/modules/custom/custom_forums/src/Controller/TopicController.php
+++ web/modules/custom/custom_forums/src/Controller/TopicController.php
@@ -2,53 +2,64 @@
 
 namespace Drupal\custom_forums\Controller;
 
+use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Link;
 use Drupal\Core\Url;
 use Drupal\custom_forums\Form\TopicForm;
 use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Session\AccountInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class TopicController extends ControllerBase {
+  protected $currentUser;
 
-  /**
-   * Display the topic creation form.
-   */
+  public function __construct(AccountInterface $currentUser) {
+    $this->currentUser = $currentUser;
+  }
+
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('current_user')
+    );
+  }
+
   public function add() {
-    $form = \Drupal::formBuilder()->getForm(TopicForm::class);
-    $render_array = [
-      '#theme' => 'item_list',
-      '#items' => [],
-    ];
-// Build a query for terms in the 'forums' vocabulary that have the specified fields.
-    $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery()->accessCheck(false);
-    $query->condition('vid', 'forums')
-      ->notExists('field_credit')
-      ->notExists('field_credit_category')
-      ->notExists('field_leed_version')
-      ->notExists('field_rating_system')
-      ->sort('tid', 'ASC');
+    $output = [];
+    if ($this->currentUser->isAnonymous()) {
+      $output['#theme'] = 'anonymous_message';
+    }
+    else {
+      $form = \Drupal::formBuilder()->getForm(TopicForm::class);
+      $ForumsList = [];
 
+      $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery()->accessCheck(false);
+      $query->condition('vid', 'forums')
+        ->notExists('field_credit')
+        ->notExists('field_credit_category')
+        ->notExists('field_leed_version')
+        ->notExists('field_rating_system')
+        ->sort('tid', 'ASC');
 
-// Execute the query to get term IDs.
-    $tids = $query->execute();
-    // Load the terms.
-    $forums = Term::loadMultiple($tids);
+      $tids = $query->execute();
+      $forums = Term::loadMultiple($tids);
 
-    // Populate the items in the render array with term names or other information.
       foreach ($forums as $forum) {
-        $term_name=$forum->get('name')->value;
-        // Replac 'your_topic_add_route' with the actual route to add a topic.
-        $url = Url::fromRoute('node.add',['node_type' => 'forum'])->setOption('query', ['forum_id' => $forum->id()]);
-        // Create a link using the forum's name and the generated URL.
+        $term_name = $forum->get('name')->value;
+        $url = Url::fromRoute('node.add', ['node_type' => 'forum'])
+          ->setOption('query', ['forum_id' => $forum->id()]);
         $link = Link::fromTextAndUrl($term_name, $url)->toRenderable();
-        // Add the link to the render array.
-        $render_array['#items'][] = $link;
+        $ForumsList[] = $link;
       }
 
+      $output = [
+        '#theme' => 'authenticated_forum_list',
+        '#forums' => $ForumsList,
+        '#form' => $form,
+      ];
+    }
 
-    return [
-      'item_list' => $render_array,
-      'form' => $form,
-    ];
+    return $output;
   }
 }
--- web/modules/custom/custom_forums/src/Form/TopicForm.php
+++ web/modules/custom/custom_forums/src/Form/TopicForm.php
@@ -4,7 +4,9 @@ namespace Drupal\custom_forums\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Url;
 use Drupal\node\Entity\Node;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 class TopicForm extends FormBase {
 
@@ -59,9 +61,27 @@ foreach ($vocabularies as $field_name => $vocabulary_id) {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
 
+// Retrieve selected taxonomy term IDs from the form state.
+    $selected_terms = [];
+    foreach ($form_state->getValues() as $field_name => $value) {
+      // Check if the field is a term selection.
+      if (in_array($field_name, ['leed_version', 'rating_system', 'credit_categorie', 'credit'])) {
+        // Add the selected term ID to the array.
+        $selected_terms[$field_name] = $value;
+      }
+    }
+    $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery()->accessCheck(false);
+    $query->condition('vid', 'forums')
+      ->condition('field_credit',$selected_terms['credit'])
+      ->condition('field_credit_category',$selected_terms['credit_categorie'])
+      ->condition('field_leed_version',$selected_terms['leed_version'])
+      ->condition('field_rating_system',$selected_terms['rating_system']);
+    $tids = $query->execute();
+    $tid=reset($tids);
+    $redirect_url= Url::fromRoute('node.add',['node_type' => 'forum'])->setOption('query', ['forum_id' => $tid])->toString();;
 
+    // Perform the redirect.
+    return  new RedirectResponse($redirect_url);
 
-
-    $form_state->setRedirect('<front>');
   }
 }
